37331c631fc72d334b9f858d4cd44593fb9edae2
Zafar Sadique <95178937+ZafarSadique@users.noreply.github.com>
2025-03-03 18:18:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,8 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "120"))
 # languages search
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL','')) 
+auth_channel = environ.get('AUTH_CHANNEL', '')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', ''))
 
 # bot settings
